d8c522bc22ebb4e214f5b15167dc2440f17cd45e
emcallaway <callaway@grinnell.edu>
2014-01-21 12:36:39 -0600

    Update HelloWorld.java

--- src/edu/grinnell/csc207/callaway/hello/HelloWorld.java
+++ src/edu/grinnell/csc207/callaway/hello/HelloWorld.java
@@ -3,5 +3,6 @@ package edu.grinnell.csc207.callaway.hello;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, GitHub!!!!");
+		System.out.println("Hello, Someone!");
 	} // main(String[])
 }
